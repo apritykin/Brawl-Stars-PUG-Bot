90077ebba31b63e5c1dfcf57697f0d5ab2d57f8f
apritykin <apritykin@gmail.com>
2019-02-21 12:29:00 -0500

    added embed for map list and teams

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const pugSize = 6; // Maximum amount of players in the PUG
 var pugMembers = ["person1", "person2", "person3", "person4","person5"]; // Array to hold the members in the PUG
 var teamA = [];
 var teamB = [];
+var gemMap = "";
+var ballMap = "";
+var heistMap = "";
+var gemGrabMaps = ["Hard Rock Mine", "Crystal Cavern", "Deathcap Cave", "Stone Fort", "Undermine", "Deep Siege", "Chill Cave", "Echo Chamber", "Flooded Mine"];
+var brawlBallMaps = ["Backyard Bowl", "Triple Dribble", "Pinhole Punt", "Sneaky Fields", "Super Stadium", "Puddle Splash", "Pool Party"];
+var heistMaps = ["Kaboom Canyon", "Safe Zone", "Forks Out", "Rolling Rumble", "Twist and Shoot", "Bridge Too Far", "Corner Case"];
 
 function checkPugSize(){
-    if (pugMembers.length == 6){
+    if (pugMembers.length == 5){
         //TODO Create the two teams
         console.log(`PUG IS FULL: ${pugMembers.length}`);
     }else{
@@ -37,11 +43,21 @@ function addUserPug(msg){
         .then(msg => console.log(pugMembers))
         .catch(console.error);
         createTeams();
+        pickMaps();
+        pugMatchDetail(msg);
     }
     else{
         console.log("There was an error!");        
     }
     
+}
+
+function pickMaps(){
+    // Randomly select one map from each game mode and save it in the maps array.
+    // [Gem Grab, Brawl Ball, Heist]
+    gemMap = gemGrabMaps[Math.floor(Math.random()*gemGrabMaps.length)];
+    ballMap = brawlBallMaps[Math.floor(Math.random()*brawlBallMaps.length)];
+    heistMap = heistMaps[Math.floor(Math.random()*heistMaps.length)];
 }
 
 function createTeams(){
@@ -65,6 +81,56 @@ function shuffle(a) {
         a[j] = x;
     }
     return a;
+}
+
+function pugMatchDetail(msg){
+    var host_user = pugMembers[Math.floor(Math.random()*pugMembers.length)];
+    const bsToken = client.emojis.find(emoji => emoji.name ==="bs_token");
+    const bsStar = client.emojis.find(emoji => emoji.name ==="bs_star");
+    const bsPurpleStar = client.emojis.find(emoji => emoji.name ==="bs_purple_star");
+    const bsGem = client.emojis.find(emoji => emoji.name ==="bs_gem");
+    const bsBall = client.emojis.find(emoji => emoji.name ==="bs_ball");
+    const bsHeist = client.emojis.find(emoji => emoji.name ==="bs_heist");
+    msg.channel.send({embed: {
+        color: 0xD3D3D3,
+        author: {
+          name: client.user.username,
+          icon_url: client.user.avatarURL
+        },
+        title: `PUG is Ready!`,
+        description: "Below you will find PUG details.",
+        fields: [{
+            name: `${bsToken} Host User - ${host_user}`,
+            value: "Please create a room and share the room code with those in this PUG"
+          },
+          {
+            name: `${bsStar} Team A`,
+            value: teamA.toString()
+          },
+          {
+            name: `${bsStar} Team B`,
+            value: teamB.toString()
+          },
+          {
+            name: `${bsGem} Gem Grab`,
+            value: gemMap
+          },
+          {
+            name: `${bsBall} Brawl Ball`,
+            value: ballMap
+          },
+          {
+            name: `${bsHeist} Heist`,
+            value: heistMap
+          },
+        ],
+        timestamp: new Date(),
+        footer: {
+          icon_url: client.user.avatarURL,
+          text: "Developed by: Pritykin"
+        }
+      }
+    });
 }
 
 client.on('ready', () => {
